661fc2f5f69e7c9d49bd2c6abc3a70c17d54e41b
no0ktheali3n <146536631+no0ktheali3n@users.noreply.github.com>
2023-10-08 05:30:58 -0400

    Update agent-scraper.py

--- agent-scraper.py
+++ agent-scraper.py
@@ -2,7 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_list(url, folder_name, file_name):
+def scrape_list(url, folder_name, file_name):  #takes target url, output directory, output filename as params
     """
     Find and scrape strings from a url to build a list of user agents
     """
@@ -11,11 +11,11 @@ def scrape_list(url, folder_name, file_name):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-    # Generate headers
+    # Generate header, unsophisticated method - implement rotating users in future versions
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' +
                'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    # Get the page
+    # try url
     try:
         page = requests.get(url, headers=headers, timeout=5)
     except requests.exceptions.Timeout:
@@ -26,7 +26,7 @@ def scrape_list(url, folder_name, file_name):
         soup = BeautifulSoup(page.content, 'html.parser')
         #print(f'html: {soup.prettify()}')
         
-        # Find the table
+        # Find all tables - if they exist grab data from each cell, row by row
         tables = soup.find_all('table', class_='table table-striped') # adjust class name as appropriate
         results = 0
         if tables:
@@ -34,16 +34,16 @@ def scrape_list(url, folder_name, file_name):
                 for table in tables:
                     rows = table.find_all('tr')
                     for row in rows:
-                        cells = row.find_all('td') # (['th, td']) to return header and data cells
+                        cells = row.find_all('td') # (['th, td']) to return data and header cells (in case categories/headlines etc useful)
                         if len(cells) > 0:
                             str = cells[0].get_text()
                             results+=1
                             f.write(str + '\n')
                 print(f'Wrote {results} items to {folder_name}/{file_name}')
         else:
-            print('No table found')
+            print('No tables found on this page!')
     else:
         print(f'Error accessing {url}: Status Code: {page.status_code}')
 
-# takes url target, desired output folder directory and file name as params
+# call script
 scrape_list('URL_Here', 'Folder_Here', 'Output_File.txt')  
